70191ee139b10b74dc4dea3a9bfe786eedc0964c
volody <volody@avisosoft.com>
2017-12-12 01:23:23 -0500

    added optimize function

--- soicc/my-version-01/main.py
+++ soicc/my-version-01/main.py
@@ -6,11 +6,11 @@ def updateDataset(data):
    data['inc_angle'] = pd.to_numeric(data['inc_angle'], errors='coerce')
    data['inc_angle'] = data['inc_angle'].fillna(method='pad')
 
-current_file = os.path.abspath(os.path.dirname(__file__)) 
-train_filename = os.path.join(current_file, '..\..\..\statoil-iceberg-classifier-challenge/input/train.json')
+# current_file = os.path.abspath(os.path.dirname(__file__)) 
+# train_filename = os.path.join(current_file, '..\..\..\statoil-iceberg-classifier-challenge/input/train.json')
 
-train = pd.read_json(train_filename)
-updateDataset(train)
+# train = pd.read_json(train_filename)
+# updateDataset(train)
 
 # Data fields
 # train.json, test.json
@@ -23,7 +23,7 @@ updateDataset(train)
 #     inc_angle - the incidence angle of which the image was taken. 
 #     is_iceberg - the target variable, set to 1 if it is an iceberg, and 0 if it is a ship.
 
-print(list(train)) # display columns in data frame
+# print(list(train)) # display columns in data frame
 
 # todo: implement logistic regression for gradient descent
 #  y^ = sigmoid ( W^T * x + b)
@@ -43,20 +43,32 @@ def initialize_parameters_with_zeros(dim):
 
 def forward_propagate(w, b, X, Y):
     m = X.shape[1]
-    A = sigmoid(w.T * X + b)
+    A = sigmoid(np.dot(w.T, X) + b)
     cost = - (1/m) * np.sum( Y * np.log(A) + (1 - Y) * np.log(1 - A))
     cost = np.squeeze(cost)
     assert(cost.shape == ())
-    return {"A": A, "cost": cost}
+    return A, cost
 
 def backward_propagate(w, A, X, Y):
     m = X.shape[1]    
-    dw = (1/m) * X * (A - Y).T
+    dw = (1/m) * np.dot(X, (A - Y).T)
     db = (1/m) * np.sum(A - Y)
     assert(dw.shape == w.shape)
     assert(db.dtype == float)
-    grads = {"dw": dw, "db": db}
-    return grads
+    return dw, db
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    for i in range(num_iterations):
+        A, cost = forward_propagate(w, b, X, Y)
+        dw, db = backward_propagate(w, A, X, Y)
+        w -= learning_rate * dw
+        b -= learning_rate * db
+        if i % 100 == 0:
+            costs.append(cost)
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    return w, b, dw, db, costs
 
 # version 1: implement loss function
 # L( y^, y) = (1/2) * (y^ - y)^2
