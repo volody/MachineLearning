68218823e0a9a563d6d607d5b4e5e2f79398b29d
volody <volody@avisosoft.com>
2018-08-28 01:25:15 -0400

    updated code

--- spamclassifier/demo.py
+++ spamclassifier/demo.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import re
 from html import unescape
+from sklearn.base import BaseEstimator, TransformerMixin
+from scipy.sparse import csr_matrix
 
 SPAM_PATH = "../../input/spamclassifier/"
 
@@ -88,3 +90,113 @@ html_spam_emails = [email for email in X_train[y_train==1]
 # dump sample
 sample_html_spam = html_spam_emails[7]
 # print(sample_html_spam.get_content().strip()[:100], "...")    
+
+def email_to_text(email):
+    html = None
+    for part in email.walk():
+        ctype = part.get_content_type()
+        if not ctype in ("text/plain", "text/html"):
+            continue
+        try:
+            content = part.get_content()
+        except: # in case of encoding issues
+            content = str(part.get_payload())
+        if ctype == "text/plain":
+            return content
+        else:
+            html = content
+    if html:
+        return html_to_plain_text(html)
+
+# dump text
+# print(email_to_text(sample_html_spam)[:100], "...")   
+
+# use Natural Language Toolkit (NLTK)
+try:
+    import nltk
+    stemmer = nltk.PorterStemmer()
+    # for word in ("Computations", "Computation", "Computing", "Computed", "Compute", "Compulsive"):
+    #     print(word, "=>", stemmer.stem(word))
+except ImportError:
+    print("Error: stemming requires the NLTK module.")
+    stemmer = None
+
+# use the urlextract library
+try:
+    import urlextract # may require an Internet connection to download root domain names
+    
+    url_extractor = urlextract.URLExtract()
+    #print(url_extractor.find_urls("Will it detect github.com and https://youtu.be/7Pq-S557XQU?t=3m32s"))
+except ImportError:
+    print("Error: replacing URLs requires the urlextract module.")
+    url_extractor = None    
+
+class EmailToWordCounterTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, strip_headers=True, lower_case=True, remove_punctuation=True,
+                 replace_urls=True, replace_numbers=True, stemming=True):
+        self.strip_headers = strip_headers
+        self.lower_case = lower_case
+        self.remove_punctuation = remove_punctuation
+        self.replace_urls = replace_urls
+        self.replace_numbers = replace_numbers
+        self.stemming = stemming
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X, y=None):
+        X_transformed = []
+        for email in X:
+            text = email_to_text(email) or ""
+            if self.lower_case:
+                text = text.lower()
+            if self.replace_urls and url_extractor is not None:
+                urls = list(set(url_extractor.find_urls(text)))
+                urls.sort(key=lambda url: len(url), reverse=True)
+                for url in urls:
+                    text = text.replace(url, " URL ")
+            if self.replace_numbers:
+                text = re.sub(r'\d+(?:\.\d*(?:[eE]\d+))?', 'NUMBER', text)
+            if self.remove_punctuation:
+                text = re.sub(r'\W+', ' ', text, flags=re.M)
+            word_counts = Counter(text.split())
+            if self.stemming and stemmer is not None:
+                stemmed_word_counts = Counter()
+                for word, count in word_counts.items():
+                    stemmed_word = stemmer.stem(word)
+                    stemmed_word_counts[stemmed_word] += count
+                word_counts = stemmed_word_counts
+            X_transformed.append(word_counts)
+        return np.array(X_transformed)
+
+# transform some emails
+X_few = X_train[:3]
+X_few_wordcounts = EmailToWordCounterTransformer().fit_transform(X_few)
+# print(X_few_wordcounts)
+
+class WordCounterToVectorTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, vocabulary_size=1000):
+        self.vocabulary_size = vocabulary_size
+    def fit(self, X, y=None):
+        total_count = Counter()
+        for word_count in X:
+            for word, count in word_count.items():
+                total_count[word] += min(count, 10)
+        most_common = total_count.most_common()[:self.vocabulary_size]
+        self.most_common_ = most_common
+        self.vocabulary_ = {word: index + 1 for index, (word, count) in enumerate(most_common)}
+        return self
+    def transform(self, X, y=None):
+        rows = []
+        cols = []
+        data = []
+        for row, word_count in enumerate(X):
+            for word, count in word_count.items():
+                rows.append(row)
+                cols.append(self.vocabulary_.get(word, 0))
+                data.append(count)
+        return csr_matrix((data, (rows, cols)), shape=(len(X), self.vocabulary_size + 1))
+
+
+vocab_transformer = WordCounterToVectorTransformer(vocabulary_size=10)
+X_few_vectors = vocab_transformer.fit_transform(X_few_wordcounts)
+print(X_few_vectors.toarray())
+print(vocab_transformer.vocabulary_)
